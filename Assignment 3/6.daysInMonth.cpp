bc1cca5e7ea0550c25ddd5ffb636d85d2836ef32
Abraham Murciano <abrahammurciano@gmail.com>
2018-10-22 23:00:25 +0300

    6. Days in month

---
+++ Assignment 3/6.daysInMonth.cpp
@@ -0,0 +1,29 @@
+/*
+ * File Name:				6.daysInMonth.cpp
+ * Program Description:		Prints the number of days in a given month
+ * Course Name:				Introduction to Computer Science
+ * Assignment Number:		3
+ * Question Number:			6
+ * Author:					Abraham Murciano
+ * 
+ * Created on:				Mon Oct 22 2018
+ * Last Modified on:		Mon Oct 22 2018
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	cout << "enter a number:" << endl;
+	int month;
+	cin >> month;
+	int days = 28 + (month + month/8)%2 + 2%month + 2*(1/month);
+	cout << days << endl;
+
+	return 0;
+}
+
+/*
+Output
+*/
