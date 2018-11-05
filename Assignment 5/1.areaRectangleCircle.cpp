b4d9573e96e59a410807befacd2dd5f8c8a82909
Abraham Murciano <abrahammurciano@gmail.com>
2018-11-05 16:23:18 +0200

    A5Q1

--- Assignment 5/1.areaRectangleCircle.cpp
+++ Assignment 5/1.areaRectangleCircle.cpp
@@ -10,11 +10,26 @@
  * Last Modified on:		Sun Nov 04 2018
  */
 
+#include <climits>  // Necessary to use the maximum and minimum integer values
 #include <iostream>
 using namespace std;
 
-// Declare constant Pi
-const float PI = 3.14159;
+int area(int, int);
+float area(float);
+float getArea(int);
+
+int main() {
+
+	cout << "enter length and width of the rectangle:" << endl;  // Prompt user
+	int rect = getArea(2);  // Call getArea to ask for 2 inputs. It will then call area
+	cout << rect << endl;
+
+	cout << "enter radius of the circle:" << endl;  // Prompt user
+	float circle = getArea(1);  // Call getArea to ask for 1 input. It will then call area
+	cout << circle << endl;
+
+	return 0;
+}
 
 // Function that calculates the area of a rectangle given its dimensions
 int area(int width, int height) {
@@ -31,60 +46,44 @@ int area(int width, int height) {
 // Function that calculates the area of a circle given its dimensions
 float area(float radius) {
 
-	// Check if radius is negative
-	if (radius < 0) {
+	// Check if radius is negative or 0
+	if (radius <= 0) {
 		return -1;
 	}
+
+	// Declare constant Pi
+	const float PI = 3.14159;
 
 	// Return area of circle
 	return PI * radius * radius;
 }
 
-int main() {
+// Function that reads in 1 or 2 values until valid values are inputted
+// Then it calls 'area' with the necessary number of arguments and returns the area of the shape
+float getArea(int numInputs = 1) {
+	float areaShape = 0;		 // Will store the area of the shape
+	int input1 = 0, input2 = 0;  // Will store user inputs
 
-	// Prompt user for input
-	cout << "enter length and width of the rectangle:" << endl;
-	int w, h;			// Stores user input for width and height
-	int areaRectangle;  // Stores computed area of rectangle
+	// Loop to keep asking for input until valid value(s) are given
 	while (true) {
-		cin >> w >> h;
-
-		// Call area function for rectangle
-		areaRectangle = area(w, h);
-
-		// If area returned a non-negative value, stop asking for input
-		if (areaRectangle >= 0) {
-			break;
+		if (numInputs == 1) {  // If only one input is required, the shape is a circle
+			cin >> input1;
+			// If area didn't return an error code (eg -1), break
+			if ((areaShape = area(input1)) >= 0) {
+				break;
+			}
+		} else if (numInputs == 2) {  // If only one input is required, the shape is a circle
+			cin >> input1 >> input2;
+			// If area didn't return an error code, break
+			if ((areaShape = area(input1, input2)) >= 0) {
+				break;
+			}
 		}
 
 		cout << "ERROR" << endl;
 	}
 
-	// Output the area of the rectangle
-	cout << areaRectangle << endl;
-
-	// Prompt user for input
-	cout << "enter radius of the circle:" << endl;
-	int r;			   // Stores user input for radius
-	float areaCircle;  // Stores computed area of circle
-	while (true) {
-		cin >> r;
-
-		// Call area function for rectangle
-		areaCircle = area(r);
-
-		// If area returned a non-negative value, stop asking for input
-		if (areaCircle >= 0) {
-			break;
-		}
-
-		cout << "ERROR" << endl;
-	}
-
-	// Output the area of the circle
-	cout << areaCircle << endl;
-
-	return 0;
+	return areaShape;
 }
 
 /*
