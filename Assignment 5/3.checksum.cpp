dcd29c00df48b5908eda3278b642ee300ad37937
Abraham Murciano <abrahammurciano@gmail.com>
2018-11-06 20:05:00 +0200

    A5Q3

---
+++ Assignment 5/3.checksum.cpp
@@ -0,0 +1,85 @@
+/*
+ * File Name:				3.checksum.cpp
+ * Program Description:		Computes the checksum of a social security nubmer
+ * Course Name:				Introduction to Computer Science
+ * Assignment Number:		5
+ * Question Number:			3
+ * Author:					Abraham Murciano
+ *
+ * Created on:				Tue Nov 06 2018
+ * Last Modified on:		Tue Nov 06 2018
+ */
+
+#include <iostream>
+using namespace std;
+
+int sumDigits(int);
+int checksum(int);
+
+int main() {
+
+	cout << "enter your ID:" << endl;  // Prompt user for a social security number
+
+	int social;  // Variable to store user input
+	int check;   // Variable to store checksum digit
+
+	// Read in social security number, until a valid checksum is returned
+	while (true) {
+		cin >> social;
+		check = checksum(social);
+		if (check >= 0) {
+			break;
+		}
+		cout << "ERROR" << endl;
+	}
+
+	// Output the full ID number
+	cout << "your full ID is:" << endl;
+	cout << social << check << endl;
+
+	return 0;
+}
+
+// Function that sums all the digits of parameter n
+int sumDigits(int n) {
+	int sum = 0;  // Declare sum and initialise it to 0
+
+	// Add the last digit of n to sum, then remove the last digit from n. Repeat until first digit
+	// has been added
+	do {
+		sum += n % 10;
+	} while (n /= 10);
+
+	return sum * ((sum > 0) - (sum < 0));  // Takes into account negative numbers
+}
+
+// Function that returns the checksum of 8 digit number n. If 8 digits are not provided, return -1
+int checksum(int n) {
+
+	// Check if number doesn't have 8 digits, in which case return an error code
+	if (n > 100000000 || n <= 10000000) {
+		return -1;
+	}
+
+	int sum = 0;	 // Declare sum and initialise it to 0
+	int weight = 2;  // Starts with weight as 2, it will be toggled between 1 and 2 for each digit
+
+	// Loop to go through each digit of n
+	do {
+		int digit = n % 10;				   // Get the last digit of n
+		sum += sumDigits(digit * weight);  // Add the weighted digits to the sum
+		weight = 1 + (weight == 1);		   // Toggle weight between 1 and 2
+	} while (n /= 10);
+
+	int sumLastDigit = sum % 10;	   // Get the last digit of the sum of the weighted digits
+	int checksum = 10 - sumLastDigit;  // Calculate checksum
+
+	return checksum;
+}
+
+/*
+enter your ID:
+12345678
+your full ID is:
+123456782
+*/
---
+++ Assignment 5/4.prime.cpp
@@ -0,0 +1,25 @@
+/*
+ * File Name:				4.prime.cpp
+ * Program Description:		----
+ * Course Name:				Introduction to Computer Science
+ * Assignment Number:		#
+ * Question Number:			#
+ * Author:					Abraham Murciano
+ * 
+ * Created on:				Tue Nov 06 2018
+ * Last Modified on:		Tue Nov 06 2018
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	
+
+	return 0;
+}
+
+/*
+Output
+*/
