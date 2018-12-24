205e5562b9c2825f9bad0c99d7e803d430f5e12a
Abraham Murciano <abrahammurciano@gmail.com>
2018-12-24 15:46:52 +0200

    A8 Comments

--- Assignment 8/1.buses.cpp
+++ Assignment 8/1.buses.cpp
@@ -1,6 +1,6 @@
 /*
  * File Name:				1.buses.cpp
- * Program Description:		----
+ * Program Description:		Bus line management system for Bus Lev company
  * less than 500
  * Course Name:				Introduction to Computer Science
  * Assignment Number:		8
@@ -8,7 +8,7 @@
  * Author:					Abraham Murciano
  *
  * Created on:				Tue Dec 18 2018
- * Last Modified on:		Tue Dec 18 2018
+ * Last Modified on:		Mon Dec 24 2018
  */
 
 #include <climits>
@@ -44,17 +44,19 @@ int search(int** buses, int size, int lineId, bool strict = true) {
 		}
 	}
 
-	return strict ? -1 : 0;
+	return strict ? -1 : 0;  // Return -1 if strict is true, if not, then return 0
 }
 
+// Function that resizes a dynamic array from size to newSize
 void resize(int**& arr, int size, int newSize) {
-	int** newArr = new int*[newSize];
-	int n = (size < newSize) ? size : newSize;
-	memcpy(newArr, arr, n * sizeof(int*));
-	delete[] arr;
-	arr = newArr;
+	int** newArr = new int*[newSize];			// Declare new array
+	int n = (size < newSize) ? size : newSize;  // Get smallest of the two sizes
+	memcpy(newArr, arr, n * sizeof(int*));		// Copy n elements from old to new array
+	delete[] arr;								// Delete old array
+	arr = newArr;								// Update pointer of arr
 }
 
+// Function that adds a line to the system
 void addLine(int**& buses, int& size, int lineId, int nStops, int duration) {
 
 	// Enlarge buses by 1
@@ -76,9 +78,11 @@ void addLine(int**& buses, int& size, int lineId, int nStops, int duration) {
 	size = newSize;  // Update size
 }
 
+// Function that removes a line from the system
 void deleteLine(int**& buses, int& size, int lineId) {
+	// Get the position of the line to delete
 	int pos = search(buses, size, lineId);
-	if (pos >= 0) {
+	if (pos >= 0) {  // If it isn't there, dont do anything
 		// Shift everything after lineId back one space
 		for (int** p = buses + pos; p < buses + size - 1; p++) {
 			*p = *(p + 1);
@@ -91,8 +95,11 @@ void deleteLine(int**& buses, int& size, int lineId) {
 	}
 }
 
+// Function that lists all the bus lines and their data
 void print(int** buses, int size) {
+	// Loop through every line
 	for (int** pLine = buses; pLine < buses + size; pLine++) {
+		// Loop through each field of the line
 		for (int* pElem = *pLine; pElem < *pLine + 3; pElem++) {
 			cout << *pElem << ' ';
 		}
@@ -100,29 +107,33 @@ void print(int** buses, int size) {
 	}
 }
 
+// Function that calculates the average travel time of all the bus lines
 float averageTravel(int** buses, int size) {
-	int sum = 0;
-	for (int** p = buses; p < buses + size; p++) {
-		sum += *(*p + 2);
+	int sum = 0;									// Initialise sum to 0
+	for (int** p = buses; p < buses + size; p++) {  // Loop through all the lines
+		sum += *(*p + 2);							// Add the travel time to sum
 	}
-	return (size) ? (float)sum / size : 0;
+	return (size) ? (float)sum / size : 0;  // Return sum/size, or 0 if size is 0
 }
 
+// Function that calculates the average number of stops of all the bus lines
 int averageStops(int** buses, int size) {
-	int sum = 0;
-	for (int** p = buses; p < buses + size; p++) {
-		sum += *(*p + 1);
+	int sum = 0;									// Initialise sum to 0
+	for (int** p = buses; p < buses + size; p++) {  // Loop through all the lines
+		sum += *(*p + 1);							// Add the travel time to sum
 	}
+	// Return sum/size, rounded to the nearest integer or 0 if size is 0
 	return (size) ? sum / size + (sum % size >= (float)size / 2) : 0;
 }
 
+// Function that returns the bus line with the shortest travel time
 int shortest(int** buses, int size) {
-	int line = -1;
-	int shortestTime = INT_MAX;
-	for (int** p = buses; p < buses + size; p++) {
-		if (*(*p + 2) < shortestTime) {
-			shortestTime = *(*p + 2);
-			line = **p;
+	int line = -1;				 // Set line to -1 in case there are no lines in the system
+	int shortestTime = INT_MAX;  // Set time to the largest integer
+	for (int** p = buses; p < buses + size; p++) {  // Loop through bus lines
+		if (*(*p + 2) < shortestTime) {  // If the bus line is shorter than the shortest so far
+			shortestTime = *(*p + 2);	// Update shortest
+			line = **p;					 // Update line
 		}
 	}
 	return line;
@@ -130,19 +141,21 @@ int shortest(int** buses, int size) {
 
 int main() {
 
-	int** buses = 0;
-	int nBuses = 0;
-	int choice;
+	int** buses = 0;  // Matrix to hold all buses' data
+	int nBuses = 0;   // Store the number of buses in the system
+	int choice;		  // Keep track of user's action choice
 
 	do {
 
-		cout << "enter 0-7" << endl;
+		cout << "enter 0-7" << endl;  // Prompt for input
 		cin >> choice;
 
+		// Check for invalid choice
 		if (choice < 0 || choice > 7) {
 			cout << "ERROR" << endl;
 		}
 
+		// Add a bus line
 		else if (choice == 1) {
 			int lineId;
 			cout << "enter the line to add" << endl;
@@ -159,6 +172,7 @@ int main() {
 			addLine(buses, nBuses, lineId, nStops, duration);
 		}
 
+		// Delete a bus line
 		else if (choice == 2) {
 			int lineId;
 			cout << "enter the line to delete" << endl;
@@ -167,10 +181,12 @@ int main() {
 			deleteLine(buses, nBuses, lineId);
 		}
 
+		// Print all data in the system
 		else if (choice == 3) {
 			print(buses, nBuses);
 		}
 
+		// Search for a bus line
 		else if (choice == 4) {
 			int lineId;
 			cout << "enter the line to search for" << endl;
@@ -178,14 +194,17 @@ int main() {
 			cout << search(buses, nBuses, lineId) << endl;
 		}
 
+		// Print average travel time
 		else if (choice == 5) {
 			cout << averageTravel(buses, nBuses) << endl;
 		}
 
+		// Print average number of stops
 		else if (choice == 6) {
 			cout << averageStops(buses, nBuses) << endl;
 		}
 
+		// Print the shortest travel time
 		else if (choice == 7) {
 			cout << shortest(buses, nBuses) << endl;
 		}
@@ -197,7 +216,64 @@ int main() {
 
 /*
 ========== Sample Run - start ==========
-
+enter 0-7
+1
+enter the line to add
+39
+enter the number of stops
+36
+enter the duration of the ride
+56
+enter 0-7
+1
+enter the line to add
+6
+enter the number of stops
+22
+enter the duration of the ride
+42
+enter 0-7
+1
+enter the line to add
+52
+enter the number of stops
+19
+enter the duration of the ride
+35
+enter 0-7
+2
+enter the line to delete
+6
+enter 0-7
+3
+39 36 56
+52 19 35
+enter 0-7
+4
+enter the line to search for
+39
+0
+enter 0-7
+4
+enter the line to search for
+52
+1
+enter 0-7
+4
+enter the line to search for
+6
+-1
+enter 0-7
+5
+45.5
+enter 0-7
+6
+28
+enter 0-7
+7
+52
+enter 0-7
+0
 =========== Sample Run - end ===========
 ----------------------------------------
 */
