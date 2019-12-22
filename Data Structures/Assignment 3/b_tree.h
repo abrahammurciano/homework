5438564831db611ef84bf60d674ab0c105a02b58
Abraham Murciano <abrahammurciano@gmail.com>
2019-12-22 22:20:54 +0200

    B-Trees finished insertion

--- Data Structures/Assignment 3/b_tree.h
+++ Data Structures/Assignment 3/b_tree.h
@@ -1,6 +1,5 @@
 #include <cstring>
 #include <iostream>
-using namespace std;
 
 template <class K, int O>
 class b_tree {
@@ -27,7 +26,7 @@ class b_tree {
 		node* find_leaf(const K& element) const;
 
 	  public:
-		node(int num_keys = 0, K* keys = 0, node** children = 0, node* parent = 0);
+		node(node* parent = 0, K* keys = 0, node** children = 0, int num_keys = 0);
 		// Returns number of keys in this node
 		int keys() const;
 		// Returns true if this node is the root
@@ -37,7 +36,7 @@ class b_tree {
 		// Returns a pointer to a key if found. Otherwise returns 0
 		K* search(const K& element) const;
 		// Inserts a key into the b-tree
-		void insert(const K& element, node* sub_tree = 0);
+		node* insert(const K& element, node* sub_tree = 0);
 	};
 
 	node* root;
@@ -52,23 +51,23 @@ class b_tree {
 };
 
 template <class K, int O>
-b_tree<K, O>::node::node(int num_keys, K* keys, node** children, node* parent)
+b_tree<K, O>::node::node(node* parent, K* keys, node** children, int num_keys)
 	: parent(parent), num_keys(num_keys) {
 	if (num_keys > b_tree<K, O>::MAX_KEYS) {
-		throw string("Error: Could not create node - too many keys. ") + string("Expected up to ") + to_string(MAX_KEYS) + string(", but received ") + to_string(num_keys) + '.';
+		throw std::string("Error: Could not create node - too many keys. ") + std::string("Expected up to ") + std::to_string(MAX_KEYS) + std::string(", but received ") + std::to_string(num_keys) + '.';
 	}
 	if (num_keys < b_tree<K, O>::MIN_KEYS) {
 		if (children || parent) {
-			throw string("Error: Could not create node - too few keys for a non-root, non-leaf node. ") + string("Expected at least ") + to_string(MIN_KEYS) + string(", but received ") + to_string(num_keys) + '.';
+			throw std::string("Error: Could not create node - too few keys for a non-root, non-leaf node. ") + std::string("Expected at least ") + std::to_string(MIN_KEYS) + std::string(", but received ") + std::to_string(num_keys) + '.';
 		}
 	}
 
-	memcpy(_keys, keys, num_keys * sizeof(K));
+	std::memcpy(_keys, keys, num_keys * sizeof(K));
 	if (!children) {
 		this->children = 0;
 	} else {
 		this->children = new node*[MAX_CHILDREN];
-		memcpy(this->children, children, (num_keys + 1) * sizeof(node*));
+		std::memcpy(this->children, children, (num_keys + 1) * sizeof(node*));
 	}
 }
 
@@ -132,23 +131,32 @@ K* b_tree<K, O>::node::search(const K& element) const {
 }
 
 template <class K, int O>
-void b_tree<K, O>::node::insert(const K& element, node* sub_tree) {
+typename b_tree<K, O>::node* b_tree<K, O>::node::insert(const K& element, node* sub_tree) {
 	if (keys() < MAX_KEYS) {
 		int index = find_in_keys(element);
-		memmove(&key(index + 1), &key(index), (keys() - index) * sizeof(K));
+		std::memmove(&key(index + 1), &key(index), (keys() - index) * sizeof(K));
 		key(index) = element;
 		if (!leaf()) {
-			memmove(&child(index + 2), &child(index + 1), (keys() - index) * sizeof(node*));
+			std::memmove(&child(index + 2), &child(index + 1), (keys() - index) * sizeof(node*));
 			child(index + 1) = sub_tree;
 		}
 	} else {
 		int mid = keys() / 2;
-		node* right = new node(parent, leaf(), &key(mid + 1), &child(mid + 1), keys() - mid - 1);
+		node* right = new node(parent, &key(mid + 1), &child(mid + 1), keys() - mid - 1);
 		for (int i = 0; i <= right->keys(); ++i) {
 			right->child(i)->parent = right;
 		}
+		if (!root()) {
+			parent->insert(key(mid), right);
+		} else {
+			node** root_children = new node*[2];
+			root_children[0] = this;
+			root_children[1] = right;
+			node* root = new node(0, &key(mid), root_children, 1);
+			delete[] root_children;
+			return root;
+		}
 		num_keys = mid;
-		parent->insert(key(mid), right);
 	}
 }
 
@@ -164,5 +172,8 @@ bool b_tree<K, O>::search(const K& element) const {
 template <class K, int O>
 void b_tree<K, O>::insert(const K& element) {
 	node* leaf = find_leaf(element);
-	leaf->insert(element);
+	node* new_root = leaf->insert(element);
+	if (new_root) {
+		root = new_root;
+	}
 }
