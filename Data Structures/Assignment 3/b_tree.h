f7528158cd2c1cdf043a4513179c804edb4dc384
Abraham Murciano <abrahammurciano@gmail.com>
2019-12-23 22:50:42 +0200

    B-Tree, Finished removing from the tree (i hope)

--- Data Structures/Assignment 3/b_tree.h
+++ Data Structures/Assignment 3/b_tree.h
@@ -24,6 +24,14 @@ class b_tree {
 		int find_in_keys(const K& element, int min = 0, int max = keys()) const;
 		// Returns the leaf where a new element could be inserted
 		node* find_leaf(const K& element) const;
+		// Rebalances a tree if too few keys remain in a node after removing a key
+		node* rebalance();
+		// Given the index of a child node, this function will rotate a key between that child, this node, and the child's left sibling
+		void rotate_left(int index);
+		// Given the index of a child node, this function will rotate a key between that child, this node, and the child's right sibling
+		void rotate_right(int index);
+		// Merge the two children at index and at index+1 into one node
+		void merge_children(int index);
 
 	  public:
 		node(node* parent = 0, K* keys = 0, node** children = 0, int num_keys = 0);
@@ -38,7 +46,7 @@ class b_tree {
 		// Inserts a key into the b-tree
 		node* insert(const K& element, node* sub_tree = 0);
 		// Removes a key from the b-tree
-		void remove(const K& element, int index = -1);
+		node* remove(const K& element, int index = -1);
 	};
 
 	node* root;
@@ -129,6 +137,110 @@ typename b_tree<K, M>::node* b_tree<K, M>::node::find_leaf(const K& element) con
 }
 
 template <class K, int M>
+typename b_tree<K, M>::node* b_tree<K, M>::node::rebalance() {
+	if (root()) {
+		if (keys() > 0) {
+			return 0;
+		}
+		node* new_root = child(0);
+		delete this;
+		return new_root;
+	} else {
+		if (keys() >= MIN_KEYS) {
+			return 0;
+		}
+		int parent_index = parent->find_in_keys(key(keys() - 1));
+		while (parent->child(parent_index) != this) {
+			--parent_index;
+		}
+		try {
+			parent->rotate_right(parent_index - 1);
+		} catch (const std::string&) {
+			try {
+				parent->rotate_left(parent_index + 1);
+			} catch (const std::string&) {
+				if (parent_index > 0) {
+					parent->merge_children(parent_index - 1);
+				} else {
+					parent->merge_children(parent_index);
+				}
+				return parent->rebalance();
+			}
+		}
+	}
+}
+
+template <class K, int M>
+void b_tree<K, M>::node::rotate_left(int index) {
+	if (leaf()) {
+		throw std::string("Error: Cannot rotate under a leaf.");
+	}
+	if (index < 1) {
+		throw std::string("Error: Cannot rotate left from the 0th child.");
+	}
+	if (child(index - 1)->keys() >= MAX_KEYS) {
+		throw std::string("Error: Cannot rotate to a full child.");
+	}
+	if (child(index)->keys() <= MIN_KEYS) {
+		throw std::string("Error: Cannot rotate from a child with the minimum number of keys.");
+	}
+	child(index - 1)->key(keys()) = key(index - 1);
+	key(index - 1) = child(index)->key(0);
+	memmove(&(child(index)->key(0)), &(child(index)->key(1)), (child(index)->keys() - 1) * sizeof(K));
+	if (!child(index)->leaf()) {
+		child(index - 1)->child(child(index - 1)->keys() + 1) = child(index)->child(0);
+		memmove(&(child(index)->child(0)), &(child(index)->child(1)), (child(index)->keys()) * sizeof(node*));
+	}
+	++(child(index - 1)->num_keys);
+	--(child(index)->num_keys);
+}
+
+template <class K, int M>
+void b_tree<K, M>::node::rotate_right(int index) {
+	if (leaf()) {
+		throw std::string("Error: Cannot rotate under a leaf.");
+	}
+	if (index >= keys() - 1) {
+		throw std::string("Error: Cannot rotate right from the last child.");
+	}
+	if (child(index + 1)->keys() >= MAX_KEYS) {
+		throw std::string("Error: Cannot rotate to a full child.");
+	}
+	if (child(index)->keys() <= MIN_KEYS) {
+		throw std::string("Error: Cannot rotate from a child with the minimum number of keys.");
+	}
+	memmove(&(child(index + 1)->key(1)), &(child(index + 1)->key(0)), child(index + 1)->keys() * sizeof(K));
+	child(index + 1)->key(0) = key(index);
+	key(index) = child(index)->key(child(index)->keys() - 1);
+	if (!child(index)->leaf()) {
+		memmove(&(child(index + 1)->child(1)), &(child(index + 1)->child(0)), (child(index + 1)->keys() + 1) * sizeof(node*));
+		child(index + 1)->child(0) = child(index)->child(keys());
+	}
+	++(child(index + 1)->num_keys);
+	--(child(index)->num_keys);
+}
+
+template <class K, int M>
+void b_tree<K, M>::node::merge_children(int index) {
+	if (leaf()) {
+		throw std::string("Error: Cannot merge children of a leaf. A leaf has no children.");
+	}
+	if (index >= keys() - 1) {
+		throw std::string("Error: Cannot merge the last key in a node with the next one, because the next one doesn't exist.");
+	}
+	child(index)->key(keys()) = key(index);
+	memccpy(&(child(index)->key(keys() + 1)), &(child(index + 1)->key(0)), child(index + 1)->keys() * sizeof(K));
+	if (!child(index)->leaf()) {
+		memccpy(&(child(index)->child(keys() + 1)), (&(child(index + 1)->child(0))), (child(index + 1)->keys() + 1) * sizeof(node*));
+	}
+	child(index)->num_keys += 1 + child(index + 1)->keys();
+	delete child(index + 1);
+	memmove(&key(index), &key(index + 1), (keys() - index - 1) * sizeof(K));
+	memmove(&child(index + 1), &child(index + 2), (keys() - index - 1) * sizeof(node*));
+	--num_keys;
+}
+
+template <class K, int M>
 K* b_tree<K, M>::node::search(const K& element) const {
 	int index = find_in_keys(element);
 	if (index < keys() && element == key(index)) {
@@ -178,17 +290,15 @@ typename b_tree<K, M>::node* b_tree<K, M>::node::insert(const K& element, node* 
 }
 
 template <class K, int M>
-void b_tree<K, M>::node::remove(const K& element, int index) {
+typename b_tree<K, M>::node* b_tree<K, M>::node::remove(const K& element, int index) {
 	if (index < 0) {
 		index = find_in_keys(element);
 	}
 	if (leaf()) {
 		if (index < keys() && key(index) == element) {
-			memmove(&key(index), &key(index + 1), 1 * sizeof(K));
+			memmove(&key(index), &key(index + 1), (keys() - index - 1) * sizeof(K));
 			--num_keys;
-			if (keys() < MIN_KEYS) {
-				rebalance();
-			}
+			return rebalance();
 		}
 	} else {
 		if ((index < keys() && key(index) == element)) {
@@ -196,7 +306,7 @@ void b_tree<K, M>::node::remove(const K& element, int index) {
 			key(index) = leaf->key(leaf->keys() - 1);
 			leaf->remove(key(index), leaf->keys() - 1);
 		} else {
-			child(index)->remove(element);
+			return child(index)->remove(element);
 		}
 	}
 }
@@ -212,8 +322,7 @@ bool b_tree<K, M>::search(const K& element) const {
 
 template <class K, int M>
 void b_tree<K, M>::insert(const K& element) {
-	node* leaf = find_leaf(element);
-	node* new_root = leaf->insert(element);
+	node* new_root = find_leaf(element)->insert(element);
 	if (new_root) {
 		root = new_root;
 	}
@@ -221,5 +330,8 @@ void b_tree<K, M>::insert(const K& element) {
 
 template <class K, int M>
 void b_tree<K, M>::remove(const K& element) {
-	root->remove(element);
+	node* new_root = root->remove(element);
+	if (new_root) {
+		root = new_root;
+	}
 }
