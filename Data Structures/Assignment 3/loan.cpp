e9a851b3c334b626a85e262d9f3daa66c4e96c11
Abraham Murciano <abrahammurciano@gmail.com>
2019-12-31 05:52:09 +0200

    B-Tree, loan class

---
+++ Data Structures/Assignment 3/loan.cpp
@@ -0,0 +1,79 @@
+#include "loan.h"
+using namespace std;
+
+loan::loan(int _borrower_id, string _name, date _loan_date, int _item_id)
+	: _borrower_id(_borrower_id), _name(_name), _loan_date(_loan_date), _item_id(_item_id) {}
+
+int loan::borrower_id() const {
+	return _borrower_id;
+}
+
+std::string loan::name() const {
+	return _name;
+}
+
+date loan::loan_date() const {
+	return _loan_date;
+}
+
+int loan::item_id() const {
+	return _item_id;
+}
+
+void loan::borrower_id(int _borrower_id) {
+	this->_borrower_id = _borrower_id;
+}
+
+void loan::name(std::string _name) {
+	this->_name = _name;
+}
+
+void loan::loan_date(date _loan_date) {
+	this->_loan_date = _loan_date;
+}
+
+void loan::item_id(int _item_id) {
+	this->_item_id = _item_id;
+}
+
+bool loan::operator==(const loan& l) const {
+	return borrower_id() == l.borrower_id() && name() == l.name() && loan_date() == l.loan_date() && item_id() == l.item_id();
+}
+
+bool loan::operator!=(const loan& l) const {
+	return !(*this == l);
+}
+
+bool loan::operator<(const loan& l) const {
+	if (loan_date() < l.loan_date()) {
+		return true;
+	}
+	if (item_id() < l.item_id()) {
+		return true;
+	}
+	if (borrower_id() < l.borrower_id()) {
+		return true;
+	}
+	return false;
+}
+
+bool loan::operator<=(const loan& l) const {
+	return *this < l || *this == l;
+}
+
+bool loan::operator>(const loan& l) const {
+	return !(*this <= l);
+}
+
+bool loan::operator>=(const loan& l) const {
+	return !(*this < l);
+}
+
+istream& operator>>(istream& in, loan& l) {
+	cout << "enter id name date item ";
+	return in >> l._borrower_id >> l._borrower_id >> l._loan_date >> l._item_id;
+}
+
+ostream& operator<<(ostream& out, loan& l) {
+	return out << "Lending details: id: " << l.borrower_id << " name: " << l.borrower_id << " date: " << l.loan_date << " item: " << l.item_id << endl;
+}
---
+++ Data Structures/Assignment 3/loan.h
@@ -0,0 +1,34 @@
+#include "date.h"
+#include <iostream>
+
+class loan {
+	friend std::istream& operator>>(std::istream& in, loan& l);
+	friend std::ostream& operator<<(std::ostream& out, loan& l);
+
+  private:
+	int _borrower_id;
+	std::string _name;
+	date _loan_date;
+	int _item_id;
+
+  public:
+	loan(int _borrower_id, std::string _name, date _loan_date, int _item_id);
+	int borrower_id() const;
+	std::string name() const;
+	date loan_date() const;
+	int item_id() const;
+	void borrower_id(int _borrower_id);
+	void name(std::string _name);
+	void loan_date(date _date);
+	void item_id(int _item_id);
+
+	bool operator==(const loan& l) const;
+	bool operator!=(const loan& l) const;
+	bool operator<(const loan& l) const;
+	bool operator<=(const loan& l) const;
+	bool operator>(const loan& l) const;
+	bool operator>=(const loan& l) const;
+};
+
+std::istream& operator>>(std::istream& in, loan& l);
+std::ostream& operator<<(std::ostream& out, loan& l)
