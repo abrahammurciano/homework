93f2401f5d4a4ecaed046ad4df44f519154e1950
Abraham Murciano <abrahammurciano@gmail.com>
2020-01-14 17:21:07 +0200

    Fixed memory out of bounds bug

--- Intro to Computer Science/Assignment 9/1.crypto.cpp
+++ Intro to Computer Science/Assignment 9/1.crypto.cpp
@@ -17,7 +17,7 @@ using namespace std;
 // Function that resizes a dynamic array from size to newSize
 void resize(char*& arr, int size, int newSize) {
 	char* newArr = new char[newSize];			// Declare new array
-	int n = (size < newSize) ? size : newSize;  // Get smallest of the two sizes
+	int n = (size < newSize) ? size : newSize;	// Get smallest of the two sizes
 	// Copy n elements from old to new array
 	for (int i = 0; i < n; i++) {
 		newArr[i] = arr[i];
@@ -31,23 +31,22 @@ void resize(char*& arr, int size, int newSize) {
 char* getText(char terminator = '\n') {
 	int i = 0;					 // Keeps track of index to be input
 	int size = 4;				 // Keeps track of size of dynamically allocated array
-	char* str = new char[size];  // Create dynamic string
+	char* str = new char[size];	 // Create dynamic string
 
 	while (true) {
-		char chtr;
-		cin >> noskipws >> chtr;  // Temporarily store input character (could be whitespace)
-		if (chtr == terminator || i == 80) {  // Stop at terminator character (or 80)
+		char c;
+		cin >> noskipws >> c;  // Temporarily store input character (could be whitespace)
+		if (i >= size) {	   // Check if array is too small to hold another character
+			// Double size of array
+			int newSize = size * 2;
+			resize(str, size, newSize);
+			size = newSize;
+		}
+		if (c == terminator || i == 80) {  // Stop at terminator character (or 80)
 			str[i] = '\0';
 			return str;
-		} else {
-			if (i >= size) {  // Check if array is too small to hold another character
-				// Double size of array
-				int newSize = size * 2;
-				resize(str, size, newSize);
-				size = newSize;
-			}
-			str[i++] = chtr;  // Put input into array
 		}
+		str[i++] = c;  // Put input into array
 	}
 }
 
@@ -56,19 +55,19 @@ char* getText(char terminator = '\n') {
 // array containing the new string
 char* crypto(char str[]) {
 	int size;
-	for (size = 1; str[size] != '\0'; size++) continue;  // Get size of str
+	for (size = 1; str[size] != '\0'; size++) continue;	 // Get size of str
 
-	char* newStr = new char[size];  // Make new array of same size
+	char* newStr = new char[size];	// Make new array of same size
 
-	for (int i = 0; str[i] != '\0'; i++) {  // Loop until null terminator is found
+	for (int i = 0; str[i] != '\0'; i++) {	// Loop until null terminator is found
 		// Check if character is between 'a' and 'z' in the ASCII table
 		if ('a' <= str[i] && str[i] <= 'z') {
-			newStr[i] = 'a' + 'z' - str[i];  // Calculate reverse character and replace it
+			newStr[i] = 'a' + 'z' - str[i];	 // Calculate reverse character and replace it
 		} else {
-			newStr[i] = str[i];  // Else copy character from old string to new string
+			newStr[i] = str[i];	 // Else copy character from old string to new string
 		}
 	}
-	return newStr;  // Return the new string
+	return newStr;	// Return the new string
 }
 
 int main() {
@@ -76,7 +75,7 @@ int main() {
 	cout << "enter a string: " << flush;  // Prompt for input
 	char* str = getText();				  // Read in until user presses enter
 
-	char* newStr = crypto(str);  // Encode str
+	char* newStr = crypto(str);	 // Encode str
 	delete[] str;
 
 	// Print processes string
