b34fbc6c2afa0218cc45abeefd793bc5ac0a9f22
Abraham Murciano <abrahammurciano@gmail.com>
2021-04-11 08:57:44 +0300

    Better sentence parsing

--- 3rd-Year/UNIX/HW3---shell-awk-sed/q5.sh
+++ 3rd-Year/UNIX/HW3---shell-awk-sed/q5.sh
@@ -3,6 +3,6 @@
 # Program which will capitalize the first letter of all sentences in all files in the directory txt.
 
 for file in txt/*; do
-    replaced=$(< "$file" tr '\n' '\r' | sed -Ee 's/(^|\.\s+)([a-z])/\1\U\2/g' | tr '\r' '\n')
+    replaced=$(< "$file" tr '\n' '\r' | sed -Ee 's/((?:^|[.!?]['")\]’]?\s+)['‘"([]?)([a-z])/\1\U\2/g' | tr '\r' '\n')
     echo "$replaced" > "$file"
 done
