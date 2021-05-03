7b8f142cb53fe6b365ea1f0b1b5f837181681a3b
Abraham Murciano <abrahammurciano@gmail.com>
2021-05-03 21:16:31 +0300

    appeased mypy

--- 3rd-Year/Intro-to-IT-Security/HW2---Encryption/HW2-Bonus.py
+++ 3rd-Year/Intro-to-IT-Security/HW2---Encryption/HW2-Bonus.py
@@ -1,11 +1,12 @@
 # At first the homework was unclear to me and I thought we had to make a program that will crack an actual substitution cypher. I had already done most of it, so I am submitting it anyway.
 
 from itertools import permutations
+from typing import Any, Dict, Optional
 
 # message - an encrypted string that is to be decrypted
 # chars - an iterable of characters in order of frequency from most frequent to least frequent
 # given - a dictionary of character translations we already know
-def decrypt(message, chars, given = {}):
+def decrypt(message, chars, given={}):
 	message = message.lower()
 	unknown_chars = list(filter(lambda char: char not in given.keys(), chars))
 	# frequencies is a dictionary mapping from each character to its frequency (initialised to 0)
@@ -20,7 +21,10 @@ def decrypt(message, chars, given = {}):
 	chars = list(filter(lambda char: char not in given.values(), chars))[::-1]
 
 	# frequencies_list is an array of the unknown characters in the message sorted by the frequency (lowest first) in which they appear in the message
-	frequencies_list = sorted(filter(lambda char: frequencies[char] != 0, frequencies.keys()), key = lambda char: frequencies[char])
+	frequencies_list = sorted(
+		filter(lambda char: frequencies[char] != 0, frequencies.keys()),
+		key=lambda char: frequencies[char],
+	)
 
 	# Try mapping different permutations of the frequencies
 	for permutation in permutations(chars):
@@ -29,7 +33,9 @@ def decrypt(message, chars, given = {}):
 		for char in message:
 			if char in frequencies_list:
 				# get a char from permutation in the corresponding index (from the end) where char is in frequencies_list
-				decrypted += permutation[frequencies_list.index(char) - len(frequencies_list)]
+				decrypted += permutation[
+					frequencies_list.index(char) - len(frequencies_list)
+				]
 			elif char in given.keys():
 				decrypted += given[char]
 			else:
@@ -40,14 +46,18 @@ def decrypt(message, chars, given = {}):
 
 message = input("Enter an encrypted message:\n")
 chars = "etaoinshrdlcumwfgypbvkjxqz"
-assumptions = {}
+assumptions: Dict[str, str] = {}
 generator = decrypt(message, chars)
 while True:
 	possible_decryption = next(generator)
 	print()
 	print(possible_decryption)
 	print()
-	command = input("To try another combination press enter.\nOr you may enter some assumptions in the form \"a=b,c=?,d=d\" (use ? to remove an assumption, use a=a to lock a in its place).\nTo exit, enter 0.\n")
+	command = input(
+		"To try another combination press enter.\nOr you may enter some assumptions in"
+		' the form "a=b,c=?,d=d" (use ? to remove an assumption, use a=a to lock a in'
+		" its place).\nTo exit, enter 0.\n"
+	)
 
 	if command == "0":
 		break
@@ -55,16 +65,20 @@ while True:
 	if command == "":
 		continue
 
-	assumptions = {possible_decryption[message.index(k)]:v for (k,v) in filter(lambda kv: kv[0] in message, assumptions.items())}
+	assumptions = {
+		possible_decryption[message.index(k)]: assumptions[k]
+		for k in assumptions
+		if k in message
+	}
 
-	for assumption in command.split(','):
-		kv_pair = assumption.split('=')
-		if kv_pair[1] == '?':
+	for assumption in command.split(","):
+		kv_pair = assumption.split("=")
+		if kv_pair[1] == "?":
 			assumptions.pop(kv_pair[0], None)
 		else:
 			key = kv_pair[0].strip()[0].lower()
 			value = kv_pair[1].strip()[0].lower()
-			if 'a' <= key <= 'z' and 'a' <= value <= 'z':
+			if "a" <= key <= "z" and "a" <= value <= "z":
 				# if the assumption's lhs is already used, remove it from the assumptions
 				for k, v in assumptions.items():
 					if v == value:
