902cd258df7c9180c0f154143412e955104a05db
Abraham Murciano <abrahammurciano@gmail.com>
2021-08-11 15:27:11 +0300

    tidied q8

--- 4th-Year/Advanced-C-Workshop/src/Exercise01/Ex01.08/not_quite_the_edges.c
+++ 4th-Year/Advanced-C-Workshop/src/Exercise01/Ex01.08/not_quite_the_edges.c
@@ -14,14 +14,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef bool (*BinaryIntPredicate)(int, int);
+typedef int (*IntProducer) ();
 
- /**
-  * @brief Check if a is less than b.
-  *
-  * @param a An integer to compare.
-  * @param b An integer to compare.
-  * @return true if a < b, false otherwise
-  */
+/**
+ * @brief Check if a is less than b.
+ *
+ * @param a An integer to compare.
+ * @param b An integer to compare.
+ * @return true if a < b, false otherwise
+ */
 bool LessThan(int a, int b)
 {
     return a < b;
@@ -59,7 +61,7 @@ void Swap(int *a, int *b)
  * @param b The address of the second number.
  * @param predicate A predicate that takes two integers which determines if a and b should be swapped.
  */
-void SwapIf(int *a, int *b, bool (*predicate)(int, int))
+void SwapIf(int *a, int *b, BinaryIntPredicate predicate)
 {
     if (predicate(*a, *b))
     {
@@ -68,27 +70,72 @@ void SwapIf(int *a, int *b, bool (*predicate)(int, int))
 
 }
 
-int main()
+/**
+ * @brief Given two ineger pointers and an integer, it puts the most extreme of the three integers into `*edge` and the second most extreme into `*almostEdge`. Extremity is determined according to the given `predicate`.
+ *
+ * @param edge A pointer to the most extreme of the three numbers. If `input` is more extreme, its value is updated to that of `input`.
+ * @param almostEdge A pointer to the second most extreme number. If `input` is more extreme, its value will be updated to the median of the three ints.
+ * @param input The new number to consider.
+ * @param predicate A function which takes two integers and returns true if the first integer is more extreme than the second, or false otherwise.
+ */
+void UpdateEdge(int *edge, int *almostEdge, int input, BinaryIntPredicate predicate)
+{
+    SwapIf(&input, edge, predicate);
+    SwapIf(&input, almostEdge, predicate);
+}
+
+/**
+ * @brief Takes input from `producer` until a `terminator` is inputted. Places the second smallest and second largest values into the given addresses.
+ *
+ * @param secondSmallest The address where the second smallest input should be stored.
+ * @param secondLargest The address where the second largest input should be stored.
+ * @param producer A function which takes no parameters and returns an int.
+ * @param terminator The value for which the function should end when equal to the input.
+ * @return int 0 on success. -1 if too few (less than two) inputs were received before receiving the terminator.
+ */
+int NotQuiteTheEdges(int *secondSmallest, int *secondLargest, IntProducer producer, int terminator)
 {
     int smallest = INT_MAX;
-    int secondSmallest = INT_MAX;
     int largest = INT_MIN;
-    int secondLargest = INT_MIN;
-    int inputA, inputB;
+    *secondSmallest = INT_MAX;
+    *secondLargest = INT_MIN;
 
-    while (true)
+    for (int count = 0; true; ++count)
     {
-        printf("Enter a number:\n");
-        scanf("%d", &inputA);
-        if (inputA == 0)
+        int input = producer();
+        if (input == terminator)
         {
-            break;
+            return count < 2 ? -1 : 0;
         }
-        inputB = inputA;
-        SwapIf(&inputA, &smallest, LessThan);
-        SwapIf(&inputA, &secondSmallest, LessThan);
-        SwapIf(&inputB, &largest, GreaterThan);
-        SwapIf(&inputB, &secondLargest, GreaterThan);
+        UpdateEdge(&smallest, secondSmallest, input, LessThan);
+        UpdateEdge(&largest, secondLargest, input, GreaterThan);
+    }
+}
+
+/**
+ * @brief Read an integer from stdin.
+ *
+ * @return int The integer read.
+ */
+int ReadInt()
+{
+    int input;
+    printf("Enter a number: ");
+    while (scanf("%d", &input) < 1)
+    {
+        printf("Error: Unknown error occured.\nEnter a number: ");
+    };
+    return input;
+}
+
+int main()
+{
+    int secondSmallest;
+    int secondLargest;
+
+    while (NotQuiteTheEdges(&secondSmallest, &secondLargest, ReadInt, 0) != 0)
+    {
+        printf("Error: You must enter at least two numbers. Please start over.\n");
     }
 
     printf("The second largest number is: %d\n", secondLargest);
