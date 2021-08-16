f22664335220b662bd3fb46b99d28e33b4fce3df
Abraham Murciano <abrahammurciano@gmail.com>
2021-08-16 13:33:08 +0300

    q6

---
+++ 4th-Year/R-Seminar/Week-1/q6.r
@@ -0,0 +1,24 @@
+student_records <- read.csv("4th-Year/R-Seminar/Week-1/q6_student_records.csv")
+
+sex <- 0
+sex <- factor(sex, levels = c("Male", "Female"))
+disabilities <- 0
+disabilities <- factor(disabilities, levels = c("Yes", "No"))
+country <- 0
+country <- factor(
+    country,
+    levels = c(
+        "Israel",
+        "US",
+        "England",
+        "France",
+        "Switzerland",
+        "Austria",
+        "Belgium",
+        "Other"
+    )
+)
+
+rownames(student_records) <- student_records$name
+
+print(student_records)
