17f9d3b6f5b930c4627d0d30b655b29e7f598d72
Abraham Murciano <abrahammurciano@gmail.com>
2021-08-16 13:13:38 +0300

    finished q5

--- 4th-Year/R-Seminar/Week-1/q5.r
+++ 4th-Year/R-Seminar/Week-1/q5.r
@@ -2,65 +2,51 @@ student_records <- read.csv("4th-Year/R-Seminar/Week-1/q5_student_records.csv")
 
 student_records <- transform(
     student_records,
-    science_avg = apply(
-        subset(
-            student_records,
-            select = c(
-                math_grade,
-                physics_grade,
-                chemistry_grade
-            )
-        ),
-        1,
-        function(d) {
-            weighted.mean(
-                d,
-                subset(
-                    student_records,
-                    select = c(
-                        math_credits,
-                        physics_credits,
-                        chemistry_credits
-                    )
-                )
-            )
-        }
-    ),
-    social_avg = weighted.mean(
-        c(
-            psychology_grade,
-            history_grade
-        ),
-        c(
-            psychology_credits,
-            history_credits
-        )
-    ),
-    avg = weighted.mean(
-        c(
-            math_grade,
-            physics_grade,
-            chemistry_grade,
-            psychology_grade,
-            history_grade
-        ),
-        c(
-            math_credits,
-            physics_credits,
-            chemistry_credits,
-            psychology_credits,
-            history_credits
-        )
-    ),
-    sd = sd(
-        c(
-            math_grade,
-            physics_grade,
-            chemistry_grade,
-            psychology_grade,
-            history_grade
-        )
+    science_avg = (
+        math_grade * math_credits
+            + physics_grade * physics_credits
+            + chemistry_grade * chemistry_credits
+    ) / (math_credits + physics_credits + chemistry_credits),
+    social_avg = (
+        psychology_grade * psychology_credits + history_grade * history_credits
+    ) / (psychology_credits + history_credits),
+    avg = (
+        math_grade * math_credits
+            + physics_grade * physics_credits
+            + chemistry_grade * chemistry_credits
+            + psychology_grade * psychology_credits
+            + history_grade * history_credits
+    ) / (
+        math_credits
+        + physics_credits
+            + chemistry_credits
+            + psychology_credits
+            + history_credits
     )
+)
+
+sd_summand <- function(grades, credits, average) {
+    return(credits * (grades - average) * (grades - average))
+}
+
+student_records <- transform(
+    student_records,
+    sd = sqrt((
+        sd_summand(math_grade, math_credits, avg)
+        + sd_summand(physics_grade, physics_credits, avg)
+            + sd_summand(chemistry_grade, chemistry_credits, avg)
+            + sd_summand(psychology_grade, psychology_credits, avg)
+            + sd_summand(history_grade, history_credits, avg)
+    ) / (
+        (nrow(student_records) - 1) / nrow(student_records)
+            * (
+                math_credits
+                + physics_credits
+                    + chemistry_credits
+                    + psychology_credits
+                    + history_credits
+            )
+    ))
 )
 
 print(student_records)
