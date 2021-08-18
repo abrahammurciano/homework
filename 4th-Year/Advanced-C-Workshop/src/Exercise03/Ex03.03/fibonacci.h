da41874b483f27c85911637eb39947425cd64674
Abraham Murciano <abrahammurciano@gmail.com>
2021-08-18 22:06:30 +0300

    fib

---
+++ 4th-Year/Advanced-C-Workshop/src/Exercise03/Ex03.03/fibonacci.h
@@ -0,0 +1,15 @@
+#ifndef fibonacci_h
+#define fibonacci_h
+
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+/**
+ * @brief Get the nth fibonacci number.
+ *
+ * @param n The index of the fibonacci number to get.
+ * @return ull The nth fibonacci number.
+ */
+ull fibonacci(uint n);
+
+#endif
