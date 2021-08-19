391b807c4278623fe5d9cf6c4dab1671aec96625
Abraham Murciano <abrahammurciano@gmail.com>
2021-08-19 15:14:13 +0300

    improved fibonacci

--- 4th-Year/Advanced-C-Workshop/src/Exercise03/Ex03.04/main.c
+++ 4th-Year/Advanced-C-Workshop/src/Exercise03/Ex03.04/main.c
@@ -1,18 +1,18 @@
 #include "fibonacci.h"
+#include "ReadInt.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
+#include <limits.h>
 
 int main()
 {
     mpz_t nth_fib;
-    for (size_t i = 0; i <= 100; ++i)
-    {
-        fibonacci(i, nth_fib);
-        char *nth_fib_str = mpz_get_str(NULL, 10, nth_fib);
-        printf("%3lu: %21s\n", i, nth_fib_str);
-        free(nth_fib_str);
-    }
+    uint n = ReadIntInRange("Which fibonacci number do you want?\n> ", 0, INT_MAX);
+    fibonacci(n, nth_fib);
+    char *nth_fib_str = mpz_get_str(NULL, 10, nth_fib);
+    printf("%s\n", n, nth_fib_str);
+    free(nth_fib_str);
     mpz_clear(nth_fib);
 
     return 0;
